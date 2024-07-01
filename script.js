4d41731c7a904d61741f6937e5bb29ec95387b55
LeloMarei <sebolelo.marei@gmail.com>
2024-07-01 22:26:18 +0200

    added final edits to temperatures

--- script.js
+++ script.js
@@ -81,11 +81,11 @@ function displayForecast(response) {
 </div>
 <div class="weather-forecast-temperatures">
   <div class="weather-forecast-temperature">
-    <strong>${Math.round(day.temperature.maximum)}</strong>
+    <strong>${Math.round(day.temperature.maximum)}&deg</strong>
   </div>
   <div class="weather-forecast-temperature">${" "}${Math.round(
           day.temperature.minimum
-        )}</div>
+        )}&deg</div>
 </div>
 </div>
 `;
